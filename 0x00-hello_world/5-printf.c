a998eda7a4034380e314fd33f4092aa836c5b7c4
zurumlab <akanduj59@yahoo.com>
2022-09-08 16:30:21 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,8 @@
  * Return: 0
  */
 int main(void)
+
 	{
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		printf("with proper grammar, but the outcome is a piece of art\n");
 		return (0);
 	}
