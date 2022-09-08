769c4e401bdccdde8f6b4d01e37357a45931cde7
zurumlab <akanduj59@yahoo.com>
2022-09-08 15:27:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,13 @@
  *
  * Description: using the main function
  * this program prints "programming is like building a multillingual puzzle"
+ *
  * Return : 0
+ * Description: returns 0 is for succesfully compilation
  */
 
 int main(void)
 	{
-		puts("\"Programming is like building a multillingual puzzle");
+		puts("Programming is like building a multilingual puzzle);
 		return (0);
 	}
