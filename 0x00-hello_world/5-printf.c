6e81f6a802a484e4c920c6cad0f65f572224bef2
zurumlab <akanduj59@yahoo.com>
2022-09-08 16:17:09 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - prints the string in the function
+ *
+ * Description: using the main function
+ * this programm prints "with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+int main(void)
+	{
+		printf("\"with proper grammar, but the outcome is a piece of art,");
+		return (0);
+	}
