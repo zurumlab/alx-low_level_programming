e67060b7267e28ecd31e5548a42990a66add3cab
zurumlab <akanduj59@yahoo.com>
2022-09-08 16:25:29 -0700

    5-print.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,12 @@
 
 /*
  * main - prints the string in the function
- *
  * Description: using the main function
  * this programm prints "with proper grammar, but the outcome is a piece of art,
  * Return: 0
  */
 int main(void)
 	{
-		printf("\"with proper grammar, but the outcome is a piece of art,");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 	}
