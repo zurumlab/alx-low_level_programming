8e84853395d368909a14d593d37f016172cd21b5
zurumlab <akanduj59@yahoo.com>
2022-09-10 03:35:03 -0700

    added excape character

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,7 +7,7 @@
 *
 * Description: using the main function
 * this program prints "Programming is positive
-* Return: 0
+*return: 0
 */
 int main(void)
 {
@@ -22,16 +22,16 @@ int main(void)
 	/*make your decision here*/
 	if (m > 5)
 	{
-	pirintf("Last digit of %i is %i and is greater than 5", n, m);
+	pirintf("Last digit of %i is %i and is greater than 5\n", n, m);
 	}
 	else if (m == 0)
 	{
-	printf("Last digit of %i is %i and is 0", n, m);
+	printf("Last digit of %i is %i and is 0\n", n, m);
 	}
 	else if ((m < 6) && (m != 0))
 	{
 	printf("Last digit of %i is %i and is less
-	than 6 and not 0", n, m);
+	than 6 and not 0\n", n, m);
 	}
 	return (0);
 i}
