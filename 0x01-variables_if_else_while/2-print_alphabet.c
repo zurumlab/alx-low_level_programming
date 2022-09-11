bd22b6db98d9793dffc4b171877148be28018b0f
zurumlab <akanduj59@yahoo.com>
2022-09-11 04:10:07 -0700

    space was not added but ithas been added now

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -16,7 +16,7 @@ int main(void)
 	/* store the characters in array */
 	char ch[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
 	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','\n'};
+	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
 	/* loop through the array */
 	for (i = 0; i < (int) sizeof(ch); i++)
 	{
