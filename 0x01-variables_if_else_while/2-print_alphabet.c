5d5575e51e9d38a2f0703102a9a868329c399f27
zurumlab <akanduj59@yahoo.com>
2022-09-11 02:19:11 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print charaters by using putchar function
+*
+* Description: using the main function
+* this program is on low level programming with c
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	/* store the characters in array */
+	char ch[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	/* loop through the array */
+	for (i = 0; i < (int) sizeof(ch); i++)
+	{
+	putchar(ch[i]);
+	}
+	return (0);
+}
