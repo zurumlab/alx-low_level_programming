26aa7d8f547b4c8a5dce0693150a2ec463e7dd11
zurumlab <akanduj59@yahoo.com>
2022-09-11 04:00:40 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,11 +14,11 @@ int main(void)
 	int i;
 
 	/* store the characters in array */
-	char ch[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	char ch[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
 	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
 	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	/* loop through the array */
-	for (i = 0; i < (int) sizeof(ch) - 1; i++)
+	for (i = 0; i < (int) sizeof(ch); i++)
 	{
 	putchar(ch[i]);
 	}
