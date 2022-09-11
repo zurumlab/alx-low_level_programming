606caec8bd57d08f73fe13b2a6018f8788054a51
zurumlab <akanduj59@yahoo.com>
2022-09-11 04:07:55 -0700

    errors fixed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,9 +14,9 @@ int main(void)
 	int i;
 
 	/* store the characters in array */
-	char ch[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	char ch[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
 	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','\n'};
 	/* loop through the array */
 	for (i = 0; i < (int) sizeof(ch); i++)
 	{
