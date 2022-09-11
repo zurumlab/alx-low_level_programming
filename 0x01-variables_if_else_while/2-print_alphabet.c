fa112a6c45efc2192223e19041d3435b65b658f9
zurumlab <akanduj59@yahoo.com>
2022-09-11 02:28:22 -0700

    fixed character length

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,7 +14,7 @@ int main(void)
 	int i;
 
 	/* store the characters in array */
-	char ch[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	char ch[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
 	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
 	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	/* loop through the array */
