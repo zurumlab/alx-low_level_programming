2fa01d002f44252a1723134357b4b31111abf23c
zurumlab <akanduj59@yahoo.com>
2022-09-12 10:04:36 -0700

    checking

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -17,6 +17,10 @@ int main(void)
 	/* store the characters in array */
 	char ch[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
 	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',};
+	/* create another array of characters */
+	char ch2[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
 	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
 	/* loop through the first  array and print small letters */
 	for (i = 0; i < (int) sizeof(ch); i++)
@@ -24,9 +28,9 @@ int main(void)
 	putchar((ch[i]));
 	}
 	/* loop through the second array and print capital letters */
-	for (r = 0; r < (int) sizeof(ch); r++)
+	for (r = 0; r < (int) sizeof(ch2); r++)
 	{
-	putchar(toupper(ch[r]));
+	putchar(toupper(ch2[r]));
 	}
 	return (0);
 }
