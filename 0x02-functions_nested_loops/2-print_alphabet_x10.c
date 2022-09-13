32a4a0b23e2c0db24c8c7e47da64691f2950082d
zurumlab <akanduj59@yahoo.com>
2022-09-13 15:48:36 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabets by 10 times
+ *
+ * Description: using a defined function
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, r;
+
+	/* create array of charactere */
+	char ch[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
+	/* loop through the array */
+	for (i = 0; i < 10; i++)
+	{
+	for (r = 0; r < (int) sizeof(ch); r++)
+	{
+	/* call the _putchar function */
+	_putchar(ch[r]);
+	}
+	}
+	
+}
