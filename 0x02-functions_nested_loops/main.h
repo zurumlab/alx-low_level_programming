d43322a59ffd48c88f7c2390f9af44656a29f75e
zurumlab <akanduj59@yahoo.com>
2022-09-13 12:02:17 -0700

    ckecking

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,11 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
-int -putchar(char);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _isalpha(int c);
+int _isalphat(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
@@ -13,5 +13,6 @@ void jack_bauer(void);
 int add(int a, int b);
 void print_times_table(int n);
 void num(int r, int n);
+void print_to_98(int n);
 
 #endf
