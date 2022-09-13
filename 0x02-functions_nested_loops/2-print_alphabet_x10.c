1a405c361d9d7244919bedfc33242010f3b37957
zurumlab <akanduj59@yahoo.com>
2022-09-13 15:56:00 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,7 +16,7 @@ void print_alphabet_x10(void)
 	char ch[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
 	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
 	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
-	/* loop through the array */
+	/* loop through thei array */
 	for (i = 0; i < 10; i++)
 	{
 	for (r = 0; r < (int) sizeof(ch); r++)
@@ -24,6 +24,4 @@ void print_alphabet_x10(void)
 	/* call the _putchar function */
 	_putchar(ch[r]);
 	}
-	}
-	
 }
