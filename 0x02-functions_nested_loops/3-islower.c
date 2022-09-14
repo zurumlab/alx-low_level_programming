504d458fac023dcb56a19b25ea4f823d13035348
zurumlab <akanduj59@yahoo.com>
2022-09-13 21:10:06 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * islower - checks for numbers that are lower cases
+ *
+ * @c: a c program
+ *
+ * Return: 0
+ */
+int islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
