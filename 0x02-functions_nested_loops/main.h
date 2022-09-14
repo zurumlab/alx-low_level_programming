7cdc7750955d6c2ecf79edfd6ea74dd88ed5fe9d
zurumlab <akanduj59@yahoo.com>
2022-09-13 22:26:20 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+int print_last_digit(int n);
 void jack_bauer(void);
 int add(int a, int b);
 void print_times_table(int n);
