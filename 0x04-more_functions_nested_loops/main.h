ca175194478312660ea399d95b2b5f53a3c3fc3f
zurumlab <akanduj59@yahoo.com>
2022-09-16 19:21:30 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,7 +14,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
+void print_numbers(void);
 int _putchar(char);
 int _isupper(int c);
 int _isdigit(int);
