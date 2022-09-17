dafda65c98288d1aa5ff3cc8aafaa9e4bf8e4c48
zurumlab <akanduj59@yahoo.com>
2022-09-16 20:24:16 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 14 time
+ * Return: nothing
+ */
+
+void more_numbers(void)
+{
+int r, b;
+
+for (r = 0; r < 10; r++)
+{
+	for (b = 0; b < 14; b++)
+	{
+		if (b > 9)
+		{
+			putchar((b % 10) + '0');
+		}
+			putchar((b % 10) + '0');
+			}
+				putchar(10);
+				}
+					}
