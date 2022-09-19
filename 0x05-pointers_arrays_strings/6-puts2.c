6b29df403fc3a9bb6c71493d688c66f87c4f6ea5
zurumlab <akanduj59@yahoo.com>
2022-09-19 16:20:27 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,18 @@
 #include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - a function
+ * @str: str parameter
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+int len, i;
+
+len = strlen(str);
+for (i = 0; i < len; i += 2)
+_putchar(str[i]);
+_putchar('\n');
+}
