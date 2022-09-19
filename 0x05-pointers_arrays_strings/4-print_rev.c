d503e91a35cab4d5fe73e4572384dc99b32a53a5
zurumlab <akanduj59@yahoo.com>
2022-09-19 15:45:47 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,22 @@
 #include "main.h"
+#include <string.h>
+
+
+/**
+ * print_rev - prints reverse of the string
+ * @s: the string to be printed in reverse
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+int i, n;
+
+n = strlen(s);
+
+for (i = n - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,8 @@ int _putchar(int c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void print_rev(char *s);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
 
 #endif
