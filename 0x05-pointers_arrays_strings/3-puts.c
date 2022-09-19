ad3e48bbb69c3cd465ca78233b637ab863d4b77b
zurumlab <akanduj59@yahoo.com>
2022-09-19 13:55:10 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/**
+ * _puts - a function that puts a value
+ * @str: the params
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+for (; *str != '\0'; str++)
+{
+_putchar(*str);
+}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 int _putchar(int c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+void print_rev(char *s);
 
 #endif
