35e293f3001c5e678bda30f14fb3185fb26bc50c
zurumlab <akanduj59@yahoo.com>
2022-09-19 17:24:27 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,24 @@
 #include "main.h"
+
+/**
+ * *_strcpy - copy paste string
+ *
+ * @dest: destination
+ * @src: source
+ *
+ * Return:nothing
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int it = 0;
+
+while (*(src + it) != '\0')
+{
+*(dest + it) = *(src + it);
+it++;
+}
+*(dest + it) = '\0';
+
+return (dest);
+}
