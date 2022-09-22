8cab6a6a6d17a5c77c0f4894bdd52566221ec521
zurumlab <akanduj59@yahoo.com>
2022-09-22 14:35:55 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include  <string.h>
+
+/**
+ *cap_string - capitalizes all words of a string
+ *@str: param
+ *Return: character
+ */
+
+char *cap_string(char *str)
+{
+
+int index = 0;
+
+while (str[++index])
+{
+
+while ((str[index]) >= ('a') && (str[index]) <= ('z'))
+index++;
+
+if ((str[index - 1]) == (' ') || (str[index - 1]) == ('\t')
+|| (str[index - 1]) == ('\n') || (str[index - 1]) == (',')
+|| (str[index - 1]) == (';') || (str[index - 1]) == ('.')
+|| (str[index - 1]) == ('!') || (str[index - 1]) == ('?')
+|| (str[index - 1]) == ('"') || str[index - 1] == '(' || str[index - 1] == ')'
+|| (str[index - 1]) == ')'
+|| (str[index - 1]) == '{' || (str[index - 1]) == '}')
+str[index] -= 32;
+}
+return (str);
+}
