3467e79293cfd52f6a1a6f5e379d871265e77866
zurumlab <akanduj59@yahoo.com>
2022-09-22 14:46:04 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,12 +18,20 @@ while (str[++index])
 while ((str[index]) >= ('a') && (str[index]) <= ('z'))
 index++;
 
-if ((str[index - 1]) == (' ') || (str[index - 1]) == ('\t')
-|| (str[index - 1]) == ('\n') || (str[index - 1]) == (',')
-|| (str[index - 1]) == (';') || (str[index - 1]) == ('.')
-|| (str[index - 1]) == ('!') || (str[index - 1]) == ('?')
-|| (str[index - 1]) == ('"') || str[index - 1] == '(' || str[index - 1] == ')'
-|| (str[index - 1]) == '{' || (str[index - 1]) == '}')
+if ((str[index - 1]) == (' ')
+|| (str[index - 1]) == ('\t')
+|| (str[index - 1]) == ('\n') 
+|| (str[index - 1]) == (',')
+|| (str[index - 1]) == (';')
+|| (str[index - 1]) == ('.')
+|| (str[index - 1]) == ('!')
+|| (str[index - 1]) == ('?')
+|| (str[index - 1]) == ('"')
+|| str[index - 1] == '('
+|| str[index - 1] == ')'
+|| (str[index - 1]) == '{'
+|| (str[index - 1]) == '}')
+{
 str[index] -= 32;
 }
 return (str);
