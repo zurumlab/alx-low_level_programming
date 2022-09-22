f893c942c3471148e6009a8fbac2a93b6e6987f9
zurumlab <akanduj59@yahoo.com>
2022-09-22 15:58:38 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ *leet - leet encoder
+ *@str: params
+ *Return: character
+ */
+
+char *leet(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+str[i] = transform(str[i]);
+i++;
+}
+return (str);
+}
+
+/**
+ * transform - helper function
+ * @x: char to be encoded
+ * Return: the encoded char
+ */
+
+char transform(char x)
+{
+char mapping_low[25] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
+char mapping_upper[25] = {'O', 'L', 'E', 'A', '\0', '\0', 'T'};
+int i = 0;
+char replacement = x;
+while (i < 0)
+{
+if (x == mapping_low[i] || x == mapping_upper[i])
+{
+replacement = i + '0';
+break;
+}
+i++;
+}
+return (replacement);
+}
