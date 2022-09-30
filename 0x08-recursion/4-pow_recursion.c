225a04ebca374c5835f421cb383d112e7058ebed
zurumlab <akanduj59@yahoo.com>
2022-09-29 18:53:37 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function
+ *@x: param
+ *@y: param
+ *
+ * Return: number
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+return (x *  _pow_recursion(x, (y - 1)));
+}
