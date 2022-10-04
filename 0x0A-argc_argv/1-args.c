e7d125fb8114bb1c02001964a72c870ae8f7a38b
zurumlab <akanduj59@yahoo.com>
2022-10-04 01:02:36 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - a function with arguments
+ * @argc: counts the number of arguments in the function
+ * @argv: array of variables passed toba function
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
