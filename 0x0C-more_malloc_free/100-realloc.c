0f0be3edab81c893c3b36d118814f949ac410a61
zurumlab <akanduj59@yahoo.com>
2022-10-07 08:36:36 -0700

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - function
+ * @ptr: ...
+ * @old_size: ...
+ * @new_size:...
+ * Return: nothing
+ */
+
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+char *nptr;
+unsigned int i;
+
+if (new_size == old_size)
+return (ptr);
+
+if (ptr == NULL)
+{
+return (NULL);
+
+return (ptr);
+}
+else
+{
+if (new_size == 0)
+{
+free(ptr);
+return (NULL);
+}
+}
+nptr = malloc(new_size);
+if (nptr == NULL)
+return (NULL);
+
+for (i = 0; i < old_size && i < new_size; i++)
+{
+nptr[i] = ((char *) ptr)[i];
+}
+free(ptr);
+return (nptr);
+}
